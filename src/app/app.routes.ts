782bd1a01212cc40d83b459adc4c94a2981afe77
Zsolt Németh <nzsolt01@gmail.com>
2025-02-13 17:07:07 +0100

    routing and components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
+import { AboutComponent } from './shared/components/about/about.component';
+import { PackagesComponent } from './shared/components/packages/packages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'packages',
+    component: PackagesComponent,
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
